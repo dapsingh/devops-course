7f9320b7407923819a2da3c520d6cdf2d505c75f
Ubuntu <ubuntu@ip-172-31-34-222.ap-south-1.compute.internal>
2021-03-02 17:03:30 +0000

    commiting file

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 //This is Sample Java Program
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+       System.out.println("Welcome to Git");	
     }
 }
